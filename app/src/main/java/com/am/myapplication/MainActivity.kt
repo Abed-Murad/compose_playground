55dac191664e1733693e4d5c43303e11fb0a9cbd
Abed Murad <dev.abed.murad@gmail.com>
2022-08-09 13:46:44 +0400

    Hoist the name state.

--- app/src/main/java/com/am/myapplication/MainActivity.kt
+++ app/src/main/java/com/am/myapplication/MainActivity.kt
@@ -9,12 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.am.myapplication.ui.theme.AbedComposePlaygroundTheme
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +22,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AbedComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        Header("Hello Abed!")
-                    }
-                }
+                HelloScreen()
             }
+        }
+    }
+}
+
+@Composable
+
+fun HelloScreen() {
+    // A surface container using the 'background' color from the theme
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            var name by rememberSaveable { mutableStateOf("Abed") }
+            BodyContent(name = name, onNameChange = { name = it })
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Body() {
+fun BodyContent(name: String, onNameChange: (String) -> Unit) {
     Column(modifier = Modifier.padding(16.dp)) {
-        var name by remember { mutableStateOf("Abed") }
         if (name.isNotEmpty()) {
             Text(
                 text = "Hello, $name!",
@@ -49,25 +52,17 @@ fun Body() {
             )
             OutlinedTextField(
                 value = name,
-                onValueChange = { name = it },
-                label = { Text("Name") }
+                onValueChange = onNameChange,
+                label = { Text(name) }
             )
         }
     }
-}
-
-@Composable
-fun Header(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AbedComposePlaygroundTheme {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Header("Abed")
-            Body()
-        }
+        HelloScreen()
     }
 }
