99354f74bb63b9611eb4e94a0c01fcc115226628
Abed Murad <dev.abed.murad@gmail.com>
2022-08-09 13:06:10 +0400

    Update Kotlin and compose dependencies.

--- app/src/main/java/com/am/myapplication/MainActivity.kt
+++ app/src/main/java/com/am/myapplication/MainActivity.kt
@@ -3,41 +3,71 @@ package com.am.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.am.myapplication.ui.theme.AbedsComposePlaygroundTheme
+import androidx.compose.ui.unit.dp
+import com.am.myapplication.ui.theme.AbedComposePlaygroundTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AbedsComposePlaygroundTheme {
+            AbedComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Header("Hello Abed!")
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
+fun Body() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var name by remember { mutableStateOf("Abed") }
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") }
+            )
+        }
+    }
+}
+
+@Composable
+fun Header(name: String) {
     Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AbedsComposePlaygroundTheme {
-        Greeting("Android")
+    AbedComposePlaygroundTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Header("Abed")
+            Body()
+        }
     }
 }
--- app/src/main/java/com/am/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/am/myapplication/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun AbedsComposePlaygroundTheme(
+fun AbedComposePlaygroundTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
